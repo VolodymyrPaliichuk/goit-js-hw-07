ab6f176ab4b9a4e83bde935c4f840bc8f0892534
VolodymyrPaliichuk <vpalya2527@gmail.com>
2023-06-13 16:35:25 +0300

    completed-task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const addItems = galleryItems
+  .map((item) => {
+    return `<li class="gallery__item"><a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/>
+    </a></li>`;
+  })
+  .join("");
+galleryList.insertAdjacentHTML("afterbegin", addItems);
+
+const instance = basicLightbox.create(
+  `
+    <img src=""  alt=""/>`
+);
+
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  instance
+    .element()
+    .querySelector(".basicLightbox__placeholder img")
+    .setAttribute("src", `${event.target.getAttribute("data-source")}`);
+
+  instance.show();
+});
+
+galleryList.addEventListener("keydown", (event) => {
+  const keyName = event.key;
+  if (keyName === "Escape") {
+    instance.close();
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const addItems = galleryItems
+  .map((galleryItem) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${galleryItem.original}">
+      <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+   </a>
+</li>`;
+  })
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", addItems);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "A") {
+    return;
+  }
+
+  lightbox.on("show.simplelightbox", function () {
+    event.target.getAttribute("href");
+  });
+});
